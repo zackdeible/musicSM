1807d82b67d8638f81743ac2ffd919a28e99039f
Zack Deible <zackdeible@gmail.com>
2020-04-17 17:53:14 -0400

    api and auth incorporated

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('testing', views.testing, name='testing'),
+    path('musicServiceAuth', views.musicServiceAuth, name='musicServiceAuth')
 ]
