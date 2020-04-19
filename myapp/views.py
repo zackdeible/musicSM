18567ace1adaf92fba2a2ecbee6335659129e118
Zack Deible <zackdeible@gmail.com>
2020-04-19 00:36:01 -0400

    if something is wrong its this commit

--- myapp/views.py
+++ myapp/views.py
@@ -2,37 +2,39 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from django.urls import path
-import requests
+import requests, json
 
 token = ''
 
 # Create your views here.
 def home(request):
+    # Get the token code in the url
     authCode = request.GET.get('code')
 
-    global token
-    if request.method == 'GET':
-        url = "https://accounts.spotify.com/api/token"
+    if authCode != None:
 
-        payload = ('grant_type=authorization_code&code='+
-                    authCode +
-                    '&redirect_uri=http%3A//127.0.0.1%3A8000')
-        headers = {
-          'Authorization': 'Basic YTk5Njg1ZjgzNWQyNDBjYjg3OTE1OGYyMTgzYmEwMDA6ODJkZmNjMzVkN2MxNGNhNzlhOGJjOGIyN2YwYmMzMzA=',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        global token
+        if request.method == 'GET':
+            url = "https://accounts.spotify.com/api/token"
 
-        response = requests.request("POST", url, headers=headers, data = payload)
+            payload = ('grant_type=authorization_code&code='+
+                        authCode +
+                        '&redirect_uri=http%3A//127.0.0.1%3A8000')
+            headers = {
+              'Authorization': 'Basic YTk5Njg1ZjgzNWQyNDBjYjg3OTE1OGYyMTgzYmEwMDA6ODJkZmNjMzVkN2MxNGNhNzlhOGJjOGIyN2YwYmMzMzA=',
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
 
-        token = response.json()['access_token']
-        print('MY TOKEN: ',token)
+            response = requests.request("POST", url, headers=headers, data = payload)
 
-        print(response.text.encode('utf8'))
+            token = response.json()['access_token']
+            print("---------------------",token)
+            return render(request, 'myapp/search.html')
 
-    return render(request, 'myapp/search.html')
+    else:
+        return render(request, 'myapp/musicServiceAuth.html')
 
-
-def testing(request):
+def authRedirect(request):
     return redirect('https://accounts.spotify.com/en/authorize?client_id=a99685f835d240cb879158f2183ba000&redirect_uri=http:%2F%2F127.0.0.1:8000&response_type=code')
 
 def musicServiceAuth(request):
@@ -43,10 +45,9 @@ def search(request):
 
     if request.method == 'POST':
         query = request.POST['search']
-        print(query)
         authToken = token
         q = query
-        type = 'artist'
+        type = 'track'
 
         query = query.replace(' ','%20')
 
@@ -57,13 +58,27 @@ def search(request):
                 '&access_token=' +
                 authToken)
 
-
-
-
         response = requests.request("GET", url)
 
         rjson = response.json()
+        #tracks = rjson['tracks']
+        #items = tracks['items']
+        #position = items[0]
+        #print("yooooo",tracks)
+        #print("DICKKKKKK", items)
+        #print("----------------------------------", position)
+        #print("TEST", rjson['tracks']['items'][0]['album']['name'])
 
-        print(response.text.encode('utf8'))
+        #print("-------------",rjson['tracks'].items)
+        print("ALBUM", rjson['tracks']['items'][0]['album']['name'])
+        print("ARTIST",rjson['tracks']['items'][0]['artists'][0]['name'])
+        print("SONG",rjson['tracks']['items'][0]['name'])
+        print("LINK",rjson['tracks']['items'][0]['preview_url'])
+
+        #print(response.text.encode('utf8'))
 
     return render(request, 'myapp/search.html', {'searchResults':rjson})
+
+
+def playlists(request):
+    return render(request, 'myapp/playlists.html')
