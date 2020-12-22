405392474d795a28127b620c176fc330d9282c22
Zack Deible <zackdeible@gmail.com>
2020-12-22 01:55:09 -0500

    new version

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,17 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('', views.login_request, name='account-login'),
     path('register/', views.register, name='account-register'),
-    path('logout/', views.logout_request, name='account-logout')
+    path('logout/', views.logout_request, name='account-logout'),
+    path('users/', views.get_users, name='users'),
+    path('follow/', views.follow, name='follow'),
+    path('unfollow/', views.unfollow, name='unfollow'),
+    path('editProfile/', views.edit_profile, name='editProfile'),
+    path('changeDescription/', views.change_description, name='changeDescription'),
+    path('createPost/', views.create_post, name='createPost'),
+    path('savePost/', views.save_post, name='savePost'),
+    path('feed/', views.feed, name='feed'),
+    path('generateTopSongs/', views.generate_top_songs, name='generateTopSongs'),
+    path('searchSongs/', views.search_songs, name='searchSongs'),
+    path('search/', views.search, name='search')
 
 ]
